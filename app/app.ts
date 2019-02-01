6c0faad68936e19793d734abe0b7d6b34659e991
Rob Lauer <rdlauer@gmail.com>
2019-02-01 16:22:27 -0600

    first android tweaks (plenty more to come)

--- app/app.ts
+++ app/app.ts
@@ -3,10 +3,15 @@ In NativeScript, the app.ts file is the entry point to your application.
 You can use this file to perform app-level initialization, but the primary
 purpose of the file is to pass control to the app’s first module.
 */
-import * as app from "tns-core-modules/application";
-import "./bundle-config";
+import * as app from 'tns-core-modules/application';
+import './bundle-config';
+import * as frescoModule from 'nativescript-fresco';
 
-app.run({ moduleName: "app-root/app-root" });
+app.on(app.launchEvent, () => {
+  frescoModule.initialize();
+});
+
+app.run({ moduleName: 'app-root/app-root' });
 
 /*
 Do not place any code after the application has been started as it will not
