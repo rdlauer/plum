a64afc7dcc41ac76c431ef5f61ac0bbc028f45cc
Rob Lauer <rdlauer@gmail.com>
2019-02-01 16:24:34 -0600

    minor updates

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,3 @@
-/*
-In NativeScript, the app.ts file is the entry point to your application.
-You can use this file to perform app-level initialization, but the primary
-purpose of the file is to pass control to the app’s first module.
-*/
 import * as app from 'tns-core-modules/application';
 import './bundle-config';
 import * as frescoModule from 'nativescript-fresco';
@@ -12,8 +7,3 @@ app.on(app.launchEvent, () => {
 });
 
 app.run({ moduleName: 'app-root/app-root' });
-
-/*
-Do not place any code after the application has been started as it will not
-be executed on iOS.
-*/
